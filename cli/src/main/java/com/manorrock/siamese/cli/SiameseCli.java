34e0801476ec4ff1117a2ba9d954f0590f1d9346
Manfred Riem <mriem@manorrock.com>
2020-04-04 08:23:33 -0600

    Fixes issue #109 - Migrate KubernetesExecutor to Java

--- cli/src/main/java/com/manorrock/siamese/cli/SiameseCli.java
+++ cli/src/main/java/com/manorrock/siamese/cli/SiameseCli.java
@@ -51,7 +51,7 @@ public class SiameseCli {
             Executor executor = null;
             switch(arguments[0]) {
                 case "docker": executor = new DockerExecutor(); break;
-//                case "kubernetes": executor = new KubernetesExecutor(); break;
+                case "kubernetes": executor = new KubernetesExecutor(); break;
                 case "local" : executor = new LocalExecutor(); break;
                 case "ssh" : executor = new SSHExecutor(); break;
             }
